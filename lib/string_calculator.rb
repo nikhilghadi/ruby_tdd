a3a678e94cd5fac8c59c6b474338c74decd25716
nikhilghadi <nikhilghadi665@gmail.com>
2025-01-23 19:51:39 +0530

    adding simple 1 and 2 numbers in string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,5 +2,7 @@ class StringCalculator
   def add(numbers)
     numbers.strip!
     0 if numbers.empty?
+    # initializing sum with 0 for inject method for of single number in string
+    numbers.split(",").inject(0){|sum,number| sum.to_i + number.to_i}
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -12,4 +12,29 @@ RSpec.describe StringCalculator do
       expect(calculator.add(" ")).to eq(0)
     end
   end
+
+  context "Testing with 1 number" do
+    it "should return 5 for 5" do
+      calculator = StringCalculator.new
+      expect(calculator.add("5")).to eq(5)
+    end
+
+    it "should return 10 for 10" do
+      calculator = StringCalculator.new
+      expect(calculator.add("10")).to eq(10)
+    end
+  end
+  
+  context "Testing with 2 numbers" do
+    it "Should return 2 for 1, 1" do
+      calculator = StringCalculator.new
+      expect(calculator.add("1, 1")).to eq(2)
+    end
+
+    it "Should return 4 for 1, 3" do
+      calculator = StringCalculator.new
+      expect(calculator.add("3,1")).to eq(4)
+    end
+  end
+  
 end
